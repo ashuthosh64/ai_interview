ca2d39e9c8037e98b1c9d0c655ea53853c8f6ac3
ashuthosh64 <ashuthoshp64@gmail.com>
2025-04-15 10:26:14 +0530

    setup done

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Aubrey} from "next/dist/compiled/@next/font/dist/google";
+import AuthForm from "@/components/AuthForm";
+
+const Page = () => {
+    return (
+        <AuthForm type={"sign-in"}/>
+    )
+}
+export default Page
