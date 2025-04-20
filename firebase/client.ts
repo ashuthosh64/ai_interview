0ee8de4ca4c0c3a15030a40c8c34fe58263c61e8
ashuthosh64 <ashuthoshp64@gmail.com>
2025-04-20 20:36:40 +0530

    third phase

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,17 +3,19 @@ import { initializeApp , getApp , getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCTYoR82P2Btxo2MCu4k-HOJs4UybWwh1k",
-    authDomain: "mockprep-e9bf2.firebaseapp.com",
-    projectId: "mockprep-e9bf2",
-    storageBucket: "mockprep-e9bf2.firebasestorage.app",
-    messagingSenderId: "1057225758919",
-    appId: "1:1057225758919:web:7c6dac564a9ae1865806e2",
-    measurementId: "G-QBTZEVX3YR"
+    apiKey: "AIzaSyCsYg_jDDrR79lLF-MJP89ZqlA-ykUnOBw",
+    authDomain: "ai-mock-prep-92aa5.firebaseapp.com",
+    projectId: "ai-mock-prep-92aa5",
+    storageBucket: "ai-mock-prep-92aa5.firebasestorage.app",
+    messagingSenderId: "862406318776",
+    appId: "1:862406318776:web:a6614e671f276ecd98324d",
+    measurementId: "G-R9764TRXMT"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
